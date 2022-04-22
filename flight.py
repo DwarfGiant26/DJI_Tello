5f425f9935750d3f99b3159c1c22592e9b9a6051
albertusalvinjanitra@gmail.com <albertusalvinjanitra@gmail.com>
2022-04-22 10:51:37 +1000

    update

--- flight.py
+++ flight.py
@@ -4,14 +4,57 @@ import time
 import pandas as pd
 import math
 
+def move_to_height(tello,height):
+    cur_height = tello.get_state_field('z')
+    to_move = abs(height - cur_height)
+    to_move = 0 if to_move <= 10 else max(20,to_move)
+
+    if to_move >= 20:
+        if height - cur_height < 0:
+            tello.move("down",to_move)
+        else:
+            tello.move("up",to_move)
+
+def adjust_axis(axis, tello):
+    # check by how much the drone has to move
+    if axis == 'x':
+        diff = 21 - tello.get_state_field(axis)
+    else:
+        diff = 0 - tello.get_state_field(axis)
+    if abs(diff) <= 10:
+        to_move = 0
+    elif abs(diff) <= 20:
+        to_move = 20
+    else:
+        to_move = abs(diff)
+
+    # tello drone can only move at least 20 cm, so if we dont have to move it by that much we will not
+    if to_move >= 20:
+        if diff > 0:        
+            if axis == 'x':
+                tello.move("forward",to_move)
+            elif axis == 'y':
+                tello.move("left",to_move)
+        elif diff < 0:        
+            if axis == 'x':
+                tello.move("back",to_move)
+            elif axis == 'y':
+                tello.move("right",to_move)
+
+def micro_adjustments(tello):
+    adjust_axis('x',tello)
+    adjust_axis('y',tello)
+
 # defining flights parameter
 # Column, Front, Echelon, Vee,Diamond.
 formation = "front"
 movement = "forward"
 wind_speed = 1
-wind_direction = "head_wind"
+
+wind_direction = "cross_wind"
 if wind_speed == 0:
     wind_direction = "none"
+
 dist = 200
 hover_time = 5
 
@@ -22,29 +65,31 @@ start = [1,2,3,4,1]
 dest = [5,6,7,8,5]
 swarm = TelloSwarm.fromIps(ips)
 
-#pre flight
+#beginning of flight
 swarm.parallel(lambda i,tello: tello.connect())
 swarm.parallel(lambda i,tello: tello.enable_mission_pads())
 swarm.parallel(lambda i,tello: tello.takeoff())
 swarm.parallel(lambda i,tello: tello.set_mission_pad_detection_direction(2))
 
-
 # fly
+# swarm.parallel(lambda i,tello: move_to_height(tello,70))
+swarm.parallel(lambda i,tello: tello.go_xyz_speed_mid(21,0,70,100,start[i]))
 swarm.parallel(lambda i,tello: tello.move("forward",dist))
-swarm.parallel(lambda i,tello: tello.go_xyz_speed_mid(0,0,70,100,dest[i]))
+swarm.parallel(lambda i,tello: tello.go_xyz_speed_mid(21,0,70,100,dest[i]))
+# swarm.parallel(lambda i,tello: micro_adjustments(tello))
 
+# end of flight
 swarm.parallel(lambda i,tello: tello.land())
 swarm.parallel(lambda i,tello: tello.disable_mission_pads())
-
-# Tell all drones in swarm to end their instance
 swarm.parallel(lambda i, tello: tello.end())
 
-# store the log data
+# post flight
+# retrieve log data
 d=tello.STATE_LOG
 data = pd.DataFrame(d)
 
 import os
-
+# write log data into files 1 file for each drone
 # path: dataset/formation/movement/windspeed/winddirection
 parent_folder = os.path.join("dataset",formation,movement,f"wind_speed_{wind_speed}",wind_direction)
 # write data into csv
@@ -52,10 +97,3 @@ for ip in ips:
     to_write = data[data.address == ip]
     to_write.to_csv(f"{parent_folder}/drone{ip[-1]}.csv")
 
-
-# def micro_adjustments(tello):
-#     # inacuraccy threshold
-#     threshold = 2
-#     while(math.abs(tello.get_state_field('x'))> threshold and math.abs(tello.get_state_field('y')) > threshold):
-#         if tello.get_state_field('x') > 0:
-#             tello.move("back",math.abs())
--- globalxyz.py
+++ globalxyz.py
@@ -4,3 +4,43 @@ import pandas as pd
 csv_path = "dataset/front/forward/wind_speed_0/none/drone1.csv"
 df = pd.read_csv(csv_path)
 
+""" front
+forward
+1, start(0,0), end(200,0) 1,5
+2, start(0,50), end(200,50) 2,6
+3, start(0,100), end(200,100) 3,7
+4, start(0,150), end(200,150) 4,8
+5, start(0,200), end(200,200) 1,5
+
+hovering
+everything is the same except it does not have end node
+"""
+
+# key: <drone number,mid>
+
+pad_coor = {}
+
+# front
+pad_coor['1,1'] = (0,200)
+pad_coor['1,5'] = (200,200)
+pad_coor['2,2'] = (0,150)
+pad_coor['2,6'] = (200,150)
+pad_coor['3,3'] = (0,100)
+pad_coor['3,7'] = (200,100)
+pad_coor['4,4'] = (0,50)
+pad_coor['4,8'] = (200,50)
+pad_coor['5,1'] = (0,0)
+pad_coor['5,5'] = (200,0)
+
+for i,rows in enumerate(df):
+    pad_id = rows['mid']
+    rel_x = rows['x']
+    rel_y = rows['y']
+    drone_number = rows['ip']
+    pad_coordinate = pad_coor[f"{drone_number},{pad_id}"]
+    global_x = pad_coordinate[0] + rel_x
+    global_y = pad_coordinate[1] + rel_y
+    df['global_x'][i] = global_x
+    df['global_y'][i] = global_y
+
+df.to_csv()

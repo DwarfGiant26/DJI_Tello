599d183b27c24ac800295ead99333b6bc196ee0e
Sarah Bradley <sbra0523@uni.sydney.edu.au>
2022-06-24 13:34:02 +1000

    completed get_expected_coor function

--- scan_aruco.py
+++ scan_aruco.py
@@ -1,4 +1,5 @@
 import cv2
+import Math
 import droneblocksutils.aruco_utils
 from droneblocksutils.aruco_utils import detect_markers_in_image
 
@@ -13,7 +14,42 @@ def adjust(drone,expected_coor, global_coor):
 def pixel_to_cm(rel_coor_pixel):
     pass
 
-def get_expected_coor(instructions,time):
+def get_expected_coor(instructions,time,curr_coordinates,speed):
+    #Intakes current coordinates,speed, time elapsed, and which direction it should intake. Returns expected coordinates. 
+    # instructions=[(start x, start y), (endx, endy)]
+    start_coord = instructions[0]
+    end_coord = instructions[1]
+    dist = Math.sqrt(((start_coord[0] - end_coord[0])**2) + ((start_coord[1] - end_coord[1])**2))
+    # Seconds to complete the entire distance:
+    time_to_complete = dist/speed
+    # What percentage of the journey should have been completed so far:
+    time_elapsed = time/time_to_complete
+    # Total distance which should be travelled in the x plane so far:
+    x_travelled = time_elapsed*(abs(start_coord[0] - end_coord[0])) 
+    # Total distance which should be travelled in the y plane so far:
+    y_travelled = time_elapsed*(abs(start_coord[1] - end_coord[1]))
+    # If the drone needs to travel in increasing x coordinate values:
+    if(start_coord[0] < end_coord[0]):
+        x_expected = start_coord[0] + x_travelled
+    # Drone travels in decreasing y coordinate values. 
+    else:
+        x_expected = start_coord[0] - x_travelled
+
+    # If the drone needs to travel in increasing y coordinate values:
+    if(start_coord[1] < end_coord[1]):
+        y_expected = start_coord[1] + y_travelled
+    # Drone travels in decreasing y coordinate values.
+    else:
+        y_expected = start_coord[1] - y_travelled
+    expected_coord = (x_expected, y_expected)
+    return expected_coord
+
+
+
+
+
+
+
 
 def move_precisely(drone,flight_path,instructions):
 
